628447a4f6359853dfc4ee73c9cb8161a1089f01
marky122 <141037502+marky122@users.noreply.github.com>
2026-02-21 13:43:26 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", (req, res) => {
+  res.send("POS Inventory System is Running 🚀");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
